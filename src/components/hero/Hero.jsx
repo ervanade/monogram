68e3397a1dbd562cc391a7792a57c652d7ce8a9e
Ervan Ade Wijaya <ervanade6@gmail.com>
2022-05-23 10:30:15 +0700

    fix hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,6 +5,10 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { Link } from 'react-scroll'
 import { BiChevronDown } from 'react-icons/bi'
+import TypeAnimation from 'react-type-animation'
+
+// Import Swiper styles
+import 'swiper/css';
 
 
 const Hero = () => {
@@ -22,10 +26,37 @@ const Hero = () => {
           type="video/mp4"
         />
       </video>
-        <div className='content'>
-          <h1 data-aos="fade-up">Grow your Business</h1>
-          <h1 data-aos="fade-right">Build Your Website</h1>
-          <Link to="contact" className='mt-3 button' spy={true} smooth={true} offset={-300} duration={300} data-aos="fade-up">START A PROJECT <BiChevronDown size={40} className="text-center"/> </Link>
+        <div className='content'> 
+            <TypeAnimation
+            cursor={true}
+            sequence={[
+              'Grow Your Business',
+              2000,
+            ]}
+            wrapper="h1"
+            repeat={1}
+           />
+           <div className='build'>
+          <h1 data-aos="fade-right">Build Your</h1>
+          <div className="website" data-aos="fade-up" >
+
+          <TypeAnimation
+            cursor={true}
+            sequence={[
+              'Brand Identity',
+              3000,
+              'Marketing',
+              3000,
+              'Website',
+              2000,
+            ]}
+            wrapper="h1"
+            repeat={Infinity}
+            />
+            </div>
+           </div>
+          <Link to="contact" className='mt-5 button' spy={true} smooth={true} offset={-300} duration={300} data-aos="fade-up">START A PROJECT <BiChevronDown size={40} className="text-center"/> </Link>
+        
         </div>
           
     </div>
